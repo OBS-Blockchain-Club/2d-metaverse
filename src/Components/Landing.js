c0e2b090616e8407be93dee089031479a399cac1
LeonardoRyuta <leonardo.ryuta05@gmail.com>
2021-11-29 14:15:47 +0000

    navbar added (still to be made cleaner)

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
+
 export class Landing extends Component{
 
   constructor() {
@@ -22,19 +23,27 @@ export class Landing extends Component{
   document.getElementById("block").className = "flex items-center justify-center transform translate-y-3.5 translate-x-3.5"
 }
 
+
+
   render () {
 
     return (
+                //The color for the navbar is only temporary, change it according to what u want
       <div>
-        <div className="fixed w-screen h-10">
-            <div className="select-none pt-20 text-right transform -translate-y-20 bg-yellow-900">            
-              <Link to="/Whitepaper" className="" style={{fontFamily:"Broken Console", fontSize:"20px"}}>Whitepaper</Link> 
+
+          <div className="fixed w-screen bg-blue-200">
+            <div className="text-right">
+              <Link to="/Whitepaper" className="pr-1 hover:underline" style={{fontFamily:"Broken Console", fontSize:"20px", textDecorationColor:"#4C1D95"}}>Whitepaper</Link>
             </div>
-        </div>
-        <div className="bg-white h-screen grid grid-col-6 gap-4 k">
+          </div>
+          <div className="fixed text-purple-800" style={{fontFamily:"Broken Console",filter: "drop-shadow(2px 2px 0 #000000)", fontSize:"25px"}}>
+            <Link to="/">PIXEL NFT</Link>
+          </div>
+
+        <div className="bg-white h-screen grid grid-col-6 gap-4">
           <div className="col-start-1 col-span-4">
             <div className=" flex justify-center items-center ">
-              <div className="text-purple-800 pt-8" style={{fontFamily:"Broken Console",filter: "drop-shadow(5px 5px 0 #000000)", fontSize:"5.5rem"}}>
+              <div className="text-purple-800 pt-5 transform translate-y-1" style={{fontFamily:"Broken Console",filter: "drop-shadow(5px 5px 0 #000000)", fontSize:"5.5rem"}}>
                 PIXEL NFT
               </div>
             </div>
--- src/Components/Whitepaper.js
+++ src/Components/Whitepaper.js
@@ -15,11 +15,15 @@ export class Whitepaper extends Component {
     
         return (
           <div className="bg-black h-screen">
-          <div className="fixed w-screen h-10 ">
-            <div className="pt-20 text-right transform -translate-y-20 -translate-x-2">            
-              <Link to="/" className="text-white" style={{fontFamily:"Broken Console"}}>Home</Link> 
+            <div className="fixed w-screen bg-blue-200">
+              <div className="text-right">
+                <Link to="/Whitepaper" className="pr-1 underline" style={{fontFamily:"Broken Console", fontSize:"20px", textDecorationColor:"#4C1D95"}}>Whitepaper</Link>
+              </div>
             </div>
-          </div>
+           <div className="fixed text-purple-800" style={{fontFamily:"Broken Console",filter: "drop-shadow(2px 2px 0 #000000)", fontSize:"25px"}}>
+             <Link to="/">PIXEL NFT</Link>
+           </div>
+
             <div className="text-purple-700 pb-20 transform translate-y-5" style={{fontFamily:"Broken Console", textAlign:"left", filter: "drop-shadow(5px 5px 0 #4C1D95)", fontSize:"5.5rem"}}>
               PIXEL NFT
             </div>
