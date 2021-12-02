d59a238840c603cd256dfa63c921623984c0009b
Kesar <67405604+SoloSaber@users.noreply.github.com>
2021-12-02 16:16:29 +0000

    map

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -17,8 +17,8 @@ class Game extends Component{
       pixelSize: parseInt(
         getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
       ),
-      x: 0,
-      y: 1,
+      x: this.randomIntFromInterval(600, 800),
+      y: this.randomIntFromInterval(1400, 1700),
       monsterX: Math.random() *200,
       monsterY: Math.random() *200
     }
@@ -48,6 +48,10 @@ class Game extends Component{
       68: this.directions.right,
       83: this.directions.down,
    }
+  }
+
+  randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
   async componentDidMount () {
@@ -153,7 +157,7 @@ class Game extends Component{
       this.gameLoop()
       this.emitMovement(this.current_directions)
       this.renderOtherPlayers()
-      console.log((this.state.x - this.state.monsterX) + ' ' + (this.state.y - this.state.monsterY))
+      // console.log((this.state.x - this.state.monsterX) + ' ' + (this.state.y - this.state.monsterY))
     })
   }
 
@@ -212,7 +216,8 @@ class Game extends Component{
   render() {
     const account = this.state.account
     const shortenedAcc = this.state.account ? account.substring(0, 6) + '...' + account.substring(account.length-5, account.length) : ''
-
+    const miniX = (this.state.x/2380)*280;
+    const miniY = (this.state.y/2380)*280;
     let color = 'green'
     if(this.health <= 20) {
       color = 'red'
@@ -241,8 +246,11 @@ class Game extends Component{
                 </div>
                 <div id='playershot'></div>
             </div>
-            <div id='stats' className='text-right bg-gray-200 bg-opacity-50 rounded-md' style={{position: 'absolute', top: 0, right: 0, padding: '0.4rem 0.5rem', fontSize: '1rem', color: 'rgba(20, 20, 20, 0.6)', fontSize: '1.2rem'}}>
-              <div className='font-pixelated inline-block px-5 text-black'><p className={`text-${color}-600 inline-block`}>{this.health}</p>$LIFE</div>
+            <div className='float-right text-right items-end bg-gray-200 bg-opacity-50 rounded-md' style={{position: 'absolute', top: 2, right: 5, padding: '150px 150px', fontSize: '1rem', color: 'rgba(20, 20, 20, 0.6)', fontSize: '1.2rem'}}>
+              <div className='opacity-70 absolute top-0 right-0' style={{height: '280px', backgroundImage: 'url("https://i.imgur.com/a993R8f.png")', width:'280px', backgroundSize: '100%'}}><img className='relative' src='miniplayer.png' width='3' style={{top: miniY, left: miniX}} /></div>
+            </div>
+            <div className='float-right text-right items-end bg-gray-200 bg-opacity-50 rounded-md' style={{position: 'absolute', top: 304.5, right: 5, padding: '0.4rem 0.5rem', fontSize: '1rem', color: 'rgba(20, 20, 20, 0.6)', fontSize: '1.2rem'}}>
+              <div className=' font-pixelated inline-block px-5 text-black'><p className={`text-${color}-600 inline-block`}>{this.health}</p>$LIFE</div>
               <div className='font-pixelated inline-block text-black '>{shortenedAcc}</div>
               <br/><div className='inline-block text-black' id='coords'><div className='font-pixelated'><p className='px-10 inline-block'>X: {parseInt(this.state.x)}</p>  Y: {parseInt(-this.state.y)} </div></div>
             </div>
@@ -254,6 +262,7 @@ class Game extends Component{
                 </div>
               </div>
             </div>
+            
         </div>
     );
   }
