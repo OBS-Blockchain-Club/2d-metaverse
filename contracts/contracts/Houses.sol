e4a4a4eeaa19b5f65341fc713ec46186765c913a
Kesar <cesar.huret@gmail.com>
2021-12-15 23:26:59 +0000

    smart contracts

---
+++ contracts/contracts/Houses.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Houses is Ownable, ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter public totalHouses;
+    uint256 maxHouses;
+    string private _currentBaseURI;
+
+    struct House {
+        string code;
+        
+    }
+
+    constructor() ERC721("Houses", "Houses") {
+        maxHouses = 10000;
+    }
+
+    function mintPlayer() payable public 
+    {
+        require(msg.value >= 0.1 ether, 'Mint price is 0.1 ETH');
+        require(totalHouses.current() <= maxHouses, 'Max NFTs reached');
+        uint256 newHouseId = totalHouses.current();
+
+        totalHouses.increment();
+
+        _safeMint(msg.sender, newHouseId);
+        
+    }
+
+        function setBaseURI(string memory baseURI) public onlyOwner
+    {
+        _currentBaseURI = baseURI;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory)
+    {
+        return _currentBaseURI;
+    }
+
+
+    function fetchBalance() external onlyOwner 
+    {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}   
